ff1aee5afc3351a0a7dfdb5310105b5bedd457d5
Aditya Vyavahare <vvaditya23@icloud.com>
2022-11-07 16:42:05 +0530

    Initial commit.

---
+++ ByteCoin/Model/coinManager.swift
@@ -0,0 +1,16 @@
+//
+//  coinManager.swift
+//  ByteCoin
+//
+//  Created by ヴィヤヴャハレ・アディティア on 07/11/22.
+//
+
+import Foundation
+
+struct CoinManager {
+    
+    let baseURL = "https://rest.coinapi.io/v1/exchangerate/BTC"
+    let apiKey = "99D0C0D7-963A-41CB-B37C-7DC5C6BA89D3"
+    
+    let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
+}
